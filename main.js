642b05dd8d150b6e420852dd8e5111fb71b8147b
Aneira Gramarye <alexander.szmidt@gmail.com>
2022-11-29 15:59:22 +0100

    Add search functionality

--- main.js
+++ main.js
@@ -4,6 +4,40 @@ const listings = require('./json/listings-all.json')
 
 const PORT = 3000
 
+function getTitles(query, maxListings) {
+  let titles = listings
+  .map(listing => listing.title)
+  .filter((title, index, array) => array.findIndex(x => x.toLowerCase() === title.toLowerCase()) === index)
+  .sort()
+  .map(title => {
+    let listingsForTitle = listings
+      .filter(x => x.title.toLowerCase() === title.toLowerCase())
+      .sort(x => x.date)
+      .map(x => ({...x, date: new Date(x.date)}))
+      .map(x => ({...x, date: `${ x.date.toLocaleDateString('se') } ${ x.date.toLocaleTimeString('se').slice(0, -3) }`}))
+
+    if(maxListings) {
+      const count = listingsForTitle.length
+      listingsForTitle = listingsForTitle.slice(0, maxListings)
+
+      if(listingsForTitle.length < count) {
+        listingsForTitle.push({ date: 'More...' })
+      }
+    }
+    return {
+      title,
+      url: encodeURIComponent(title),
+      listings: listingsForTitle
+    }
+  })
+
+  if(query) {
+    titles = titles.filter(item => item.title.toLowerCase().includes(query.toLowerCase()))
+  }
+
+  return titles
+}
+
 const app = express()
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, './views'))
@@ -11,27 +45,13 @@ app.set('views', path.join(__dirname, './views'))
 app.use(express.static(path.join(__dirname, './public')))
 
 app.get('/', (req, res) => {
-  const titles = listings
-    .map(listing => listing.title)
-    .filter((title, index, array) => array.findIndex(x => x.toLowerCase() === title.toLowerCase()) === index)
-    .sort()
-    .map(title => {
-      const listingsForTitle = listings
-        .filter(x => x.title.toLowerCase() === title.toLowerCase())
-        .sort(x => x.date)
-        .slice(0, 5)
-        .map(x => ({...x, date: new Date(x.date)}))
-        .map(x => ({...x, date: `${ x.date.toLocaleDateString('se') } ${ x.date.toLocaleTimeString('se').slice(0, -3) }`}))
-      return {
-        title,
-        listings: listingsForTitle
-      }
-    })
+  console.log(req.query)
 
-  console.log(`${titles.length} records`)
-  
+  const query = req.query.query
+  const titles = query ? getTitles(query) : getTitles(null, 3)
   res.render('index', {
-    titles
+    titles,
+    query
   })
 })
 
